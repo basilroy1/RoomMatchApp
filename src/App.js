b10444d0bedda797ce234877e28b37bc580379a6
Basil Roy <basil.k.roy@hotmail.com>
2018-11-07 14:34:36 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            EDITED ON MY FORK BRANCH ON TEST ACC  <code>src/App.js</code> and save to reload.
+            EDITED ON MY FORK BRANCH ON TEST ACC basil  <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
