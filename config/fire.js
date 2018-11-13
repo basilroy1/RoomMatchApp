3926a69a9d53ae0531999254134942536fcb61c2
Evan Whelan <whelanevan6@gmail.com>
2018-11-13 13:13:43 +0000

    Added firebase config to fire.js

--- config/fire.js
+++ config/fire.js
@@ -1,12 +1,13 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: "AIzaSyBGobBA4FRWwN3_gIOYV770DWtxR3OAAQw",
+    authDomain: "roommatch-f2355.firebaseapp.com",
+    databaseURL: "https://roommatch-f2355.firebaseio.com",
+    projectId: "roommatch-f2355",
+    storageBucket: "roommatch-f2355.appspot.com",
+    messagingSenderId: "776335086478"
+
 }
 
 const fire = firebase.initializeApp(config);
