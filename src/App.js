8406b49d23318db41e4f8e33ee67be50d25f0a8d
Evan Whelan <whelanevan6@gmail.com>
2018-11-13 16:49:57 +0000

    Restructured folders to put config & components folders inside src directory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import fire from '../config/fire';
-import Home from '../components/Home/Home';
-import Login from '../components/Login/Login';
+import fire from './config/fire';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
 class App extends Component {
 
