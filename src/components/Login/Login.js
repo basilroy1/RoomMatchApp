53360274ed084d7ea3f7cbb28a0b091384e66d25
Evan Whelan <whelanevan6@gmail.com>
2018-11-13 20:44:17 +0000

    Made functions to handle the access to site (LogIn/ Logout/ Sign Up)

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,77 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import fire from '../../config/fire';
 
 class Login extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = 
+    {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleChange = (e) => {
+
+    let change = {};
+    change[e.target.name] = e.target.value;
+    this.setState(change);
+
+  }
+  
+  // Allows users to sign up with email
+  handleEmailSignUp = (e) => {
+    e.preventDefault();
+
+    const newEmail = this.state.email;
+    const newPassword = this.state.password;
+    // check if user entered a mumail or mu.ie email
+    const validEmail = (newEmail.endsWith("@mumail.ie") || newEmail.endsWith("@mu.ie")); 
+
+    if(!validEmail){
+      alert("Please enter a valid maynooth email address");
+      return;
+    }
+
+    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      
+      if (errorCode == 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+    
+    });
+  }
+
+  handleEmailLogIn = () => {
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);      
+    });
+    
+  }
+
+  handleLogOut = () => {
+    fire.auth().signOut();
+  }
+  
   render() {
     return (
       <div>
