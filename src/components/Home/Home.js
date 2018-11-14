001e7dadc5e17f0c6a9ce7a1931cfe8ef294072e
Evan Whelan <whelanevan6@gmail.com>
2018-11-14 13:11:09 +0000

    Fixed login and added logout functionality

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import fire from '../../config/fire';
 
 class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.logout = this.logout.bind(this);
+  }
+
+  logout(e) {
+    e.preventDefault();
+    fire.auth().signOut();
+  }
+
   render() {
     return (
       <div>
         <h1>This is the homepage</h1>
+        <button onClick = {this.logout}>Log Out</button>
       </div>
     )
   }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,85 +2,56 @@ import React, { Component } from 'react';
 import fire from '../../config/fire';
 
 class Login extends Component {
-
   constructor(props) {
     super(props);
-    this.state = 
-    {
+    this.login = this.login.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.signup = this.signup.bind(this);
+    this.state = {
       email: '',
       password: ''
-    }
+    };
   }
 
-  handleChange = (e) => {
-    e.preventDefault();
-    let change = {};
-    change[e.target.name] = e.target.value;
-    this.setState(change);
-
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
-  
-  // Allows users to sign up with email
-  handleEmailSignUp = (e) => {
+
+  login(e) {
     e.preventDefault();
+    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+    }).catch((error) => {
+        console.log(error);
+      });
+  }
 
-    const newEmail = this.state.email;
-    const newPassword = this.state.password;
-    // check if user entered a mumail or mu.ie email
-    const validEmail = (newEmail.endsWith("@mumail.ie") || newEmail.endsWith("@mu.ie")); 
-
-    if(!validEmail){
-      alert("Please enter a valid maynooth email address");
-      return;
-    }
-
-    fire.auth().createUserWithEmailAndPassword(newEmail, newPassword)
+  signup(e){
+    e.preventDefault();
+    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+    }).then((u)=>{console.log(u)})
     .catch((error) => {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      
-      if (errorCode == 'auth/weak-password') {
-        alert('The password is too weak.');
-      } else {
-        alert(errorMessage);
-      }
-      console.log(error);
-    
-    });
+        console.log(error);
+      })
   }
-
-  handleEmailLogIn = (e) => {
-    e.preventDefault();
-
-    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-    .catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      
-      if (errorCode === 'auth/wrong-password') {
-        alert('Wrong password.');
-      } else {
-        alert(errorMessage);
-      }
-      console.log(error);      
-    });
-    
-  }
-
-  handleLogOut = (e) => {
-    e.preventDefault();
-    fire.auth().signOut();
-  }
-  
   render() {
     return (
-      <div>
-        <h1>This is the Login / Signup Page</h1>
+      <div className="col-md-6">
+        <form>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input  value={this.state.email} onChange={this.handleChange} type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input  value={this.state.password} onChange={this.handleChange} type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+          </div>
+          <button type="submit" onClick={this.login} class="btn btn-primary">Login</button>
+          <button onClick={this.signup} style={{marginLeft: '25px'}} className="btn btn-success">Signup</button>
+        </form>
+      
       </div>
-    )
+    );
   }
 }
-
 export default Login;
