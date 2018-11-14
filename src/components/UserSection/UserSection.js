c25fb35c7730f504d5768c6ce43f287218216486
basil roy <basil.k.roy@hotmail.com>
2018-11-14 12:05:16 +0000

    added form

--- src/components/UserSection/UserSection.js
+++ src/components/UserSection/UserSection.js
@@ -4,7 +4,34 @@ import React, { Component } from 'react'
   render() {
     return (
       <div>
-         user section
+         <form id="info">
+					<label>Name</label>
+						<input type="text" placeholder="Enter Name" name="name" id="name"/>
+						<br>
+						</br>
+				
+					<label>Age</label>
+						<input type="age" placeholder="Enter Age" name="age" id="age"/>
+						<br>
+						</br>
+				
+					<label>Course</label>
+						<input type="text" placeholder="Enter Course" name="course" id="course"/>
+						<br>
+						</br>
+					<label>Location</label>
+						
+						<input type="location" placeholder="Enter Location" name="location" id="location"/>
+						<br>
+						</br>
+					<label>Intrests</label>
+				
+						<input type="text" placeholder="Enter Intrests" name="intrests" id="intrests"/>
+						<br>
+						</br>   
+						<button type="submit" >Submit</button>
+						
+					</form>
       </div>
     )
   }
