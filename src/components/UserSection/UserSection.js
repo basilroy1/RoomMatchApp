cfb0339ec04843dbd5dabaa44a724f3df041d6b5
basil roy <basil.k.roy@hotmail.com>
2018-11-14 11:41:01 +0000

    added user section folder

---
+++ src/components/UserSection/UserSection.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class UserSection extends Component {
+  render() {
+    return (
+      <div>
+        This is the user section
+      </div>
+    )
+  }
+}
+export default UserSection;
