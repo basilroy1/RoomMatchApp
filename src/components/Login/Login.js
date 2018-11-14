f5b70737d905577fb2f4102c083fb83321ee9af7
Evan Whelan <whelanevan6@gmail.com>
2018-11-14 20:47:22 +0000

    Began adding the 'remember me' functionality

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import fire from '../../config/fire';
+import Bootstrap from 'bootstrap';
 
 class Login extends Component {
   constructor(props) {
@@ -7,11 +8,15 @@ class Login extends Component {
     this.login = this.login.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.signup = this.signup.bind(this);
+    // this.togglePersistance = this.togglePersistance.bind(this);
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      persistantSession : ""
     };
   }
+
+
 
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
@@ -19,10 +24,21 @@ class Login extends Component {
 
   login(e) {
     e.preventDefault();
+    
+    const userWantsToBeRemembered = this.state.persistantSession; 
+    if(!userWantsToBeRemembered){ // if the user didn't click the 'remember me' checkbox
+      fire.auth().setPersistence("none");  // don't allow persistant log in
+    }
+
+
     fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+      console.log("Log in ");
     }).catch((error) => {
-        console.log(error);
-      });
+        
+      alert(error.message);
+
+      console.log(error.message);
+    });
   }
 
   signup(e){
@@ -35,7 +51,7 @@ class Login extends Component {
   }
   render() {
     return (
-      <div>
+      <div className="container">
         <form>
           <div className="form-group">
             <label htmlFor="exampleInputEmail1">Email address</label>
@@ -47,7 +63,8 @@ class Login extends Component {
             <input  value={this.state.password} onChange={this.handleChange} type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
           </div>
           <button type="submit" onClick={this.login}>Login</button>
-          <button onClick={this.signup} >Signup</button>
+          <button onClick={this.signup} >Signup</button> <br />
+          <input type="checkbox" name="persistantSession" onClick={this.handleChange}/>Remember me<br />
         </form>
       
       </div>
