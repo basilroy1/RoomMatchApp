920964ba585e2aed5381c9273a2032e9aa16efb7
basil roy <basil.k.roy@hotmail.com>
2018-11-14 13:58:02 +0000

    lol

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,10 @@
-import React, { Component } from 'react';
-import fire from '../../config/fire';
-import UserSection from '../UserSection/UserSection';
+import React, { Component } from 'react'
 
 class Home extends Component {
-
-  constructor(props) {
-    super(props);
-    this.logout = this.logout.bind(this);
-  }
-
-  logout(e) {
-    e.preventDefault();
-    fire.auth().signOut();
-  }
-
   render() {
     return (
       <div>
         <h1>This is the homepage</h1>
-        <UserSection />
-
-        <button onClick = {this.logout}>Log Out</button>
       </div>
     )
   }
