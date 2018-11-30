54d905ecfa704d0cc5626e842510450deae3560c
EvanWhelan <whelanevan6@gmail.com>
2018-11-30 11:10:34 +0000

    Fetched firebase data and put into state. TODO: Fix mapping function to display the data

--- src/components/TestHomePage/TestHomePage.js
+++ src/components/TestHomePage/TestHomePage.js
@@ -1,30 +1,71 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col, ListGroup, Button,  ListGroupItem, Popover, OverlayTrigger } from 'react-bootstrap';
 import './TestHomePage.css';
+import fire from '../../config/fire';
 
 class TestHomePage extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      people: [{name: "Ronan", age:"17", course:"Leaving Cert"},{name: "Evan", age: "20", course: "csse"},{name: "Niamh", age: "19", course: "Geog"},{name: "Ronan", age:"17", course:"Leaving Cert"},{name: "Evan", age: "20", course: "csse"},{name: "Niamh", age: "19", course: "Geog"}  ]
+      hardCodedPeople: [{name: "Ronan", age:"17", course:"Leaving Cert"},{name: "Evan", age: "20", course: "csse"},{name: "Niamh", age: "19", course: "Geog"},{name: "Ronan", age:"17", course:"Leaving Cert"},{name: "Evan", age: "20", course: "csse"},{name: "Niamh", age: "19", course: "Geog"}],
+      people: []
     }
   }
 
+  componentDidMount() {
+    const db = fire.database();
+    const ref = db.ref('User');
+    let currentState = this.state.people;
+    const newState = [];
+    console.log("State before data fetch --> " + currentState);
+
+
+
+    ref.on('value', (snapshot) => {
+      snapshot.forEach( (data) => {
+        const currentStudent = data.val();
+        let user ={
+          name: currentStudent.Name,
+          age: currentStudent.Age,
+          location: currentStudent.Location,
+          course: currentStudent.Course,
+          interests: currentStudent.Intrests
+        }
+        currentState.push(user);
+
+      });
+      console.log(currentState);
+    })
+    this.setState({
+      people: currentState
+    });
+  }
+
+
+
   render() {
+
+
+
 
     const popoverHoverFocus = (
       <Popover id="popover-trigger-hover-focus">
         <strong>Let's Connect!</strong>
-      </Popover>  
+      </Popover>
     );
 
-    let data = [...this.state.people];
-    let renderData = data.map((person) => 
+    // TODO: Fix the mapping functionality to render the firebase data to the screen
+
+    let data = [...this.state.hardCodedPeople];
+    console.log(data);
+    let renderData = data.map((person) =>
     <ListGroup>
       <ListGroupItem key = {person.name}><strong>Name: </strong>{person.name}</ListGroupItem>
       <ListGroupItem key = {person.age}><strong>Age: </strong>{person.age}</ListGroupItem>
+      <ListGroupItem key = {person.location}><strong>Location: </strong>{person.location}</ListGroupItem>
       <ListGroupItem key = {person.course}><strong>Course: </strong>{person.course}</ListGroupItem>
+      <ListGroupItem key = {person.interests}><strong>Interests: </strong>{person.interests}</ListGroupItem>
       <ListGroupItem>
       <OverlayTrigger
         trigger={['hover', 'focus']}
@@ -35,15 +76,15 @@ class TestHomePage extends Component {
     </OverlayTrigger>
     </ListGroupItem>
     </ListGroup>
-       
+
     );
 
     return (
       <div>
         <Grid>
           <Row >
-            <Col className="mainSection" xs={12} md={8}> 
-              {renderData}  
+            <Col className="mainSection" xs={12} md={8}>
+              {renderData}
             </Col>
             <Col className="sideSection" md={4}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat orci ac tortor pharetra, vel egestas sem semper. Vivamus at leo malesuada, lobortis ipsum vitae, blandit metus. Sed a tincidunt tellus, sed cursus est. Mauris arcu urna, consectetur eu est id, vulputate congue sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin elementum, lacus eget auctor blandit, magna justo finibus nibh, et placerat arcu turpis at massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce volutpat ornare urna vel vehicula. Maecenas ac rutrum velit. Aenean eu fermentum sapien.
@@ -54,8 +95,9 @@ class TestHomePage extends Component {
             </Col>
           </Row>
         </Grid>
-         
-              
+
+
+
       </div>
     );
   }
