41d13ad4ae145fb12d99b3218697fcc30a3d271a
EvanWhelan <whelanevan6@gmail.com>
2018-12-03 19:30:07 +0000

    Removed constructor

--- src/components/UserWindow/UserWindow.js
+++ src/components/UserWindow/UserWindow.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import { ListGroup, ListGroupItem, Button, Popover, OverlayTrigger } from 'react-bootstrap';
-
 class UserWindow extends Component {
-
-  constructor(props) {
-    super(props);
-  }
 
   render() {
 
