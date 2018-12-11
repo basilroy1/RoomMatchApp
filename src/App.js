fcd6ace14e6505c1947be595288c36afcf94ba5a
Jason Aikhionbare <falsewire@gmail.com>
2018-12-11 11:30:13 +0000

    hgi

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import fire from './config/fire';
 import Login from './components/Login/Login';
-<<<<<<< HEAD
  import UserSection from './components/UserSection/UserSection';
-=======
->>>>>>> 8f174dec6bc2af21ee36405e50d5f14f72af7005
+
 import TestHomePage from './components/TestHomePage/TestHomePage';
 
 class App extends Component {
