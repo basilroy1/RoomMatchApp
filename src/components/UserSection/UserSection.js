82ec869e1b5210cf86f5753efa90cf0ede2b93fd
Jason Aikhionbare <Falsewire@gmail.com>
2018-12-18 04:10:56 -0800

    nn

--- src/components/UserSection/UserSection.js
+++ src/components/UserSection/UserSection.js
@@ -14,50 +14,28 @@ class UserSection extends Component {
       location: "",
       interests: "",
       year: "",
-      profilePicture: 1,
-      userHasSubmitted: false
+      clickPopUp: false,
+      profilePicture: 1
     }
   }
-
-  handleImageUpload = async(e) => {
-
-    e.preventDefault();
-    const imageAsUrl = await this.encodeImageFileAsUrl(e.target.files[0]);
-    // console.log("2) imageAsUrl(handleImageUpload) = " + imageAsUrl);
-
-    this.setState({
-      profilePicture: imageAsUrl
-    });
-
-  }
-
-  encodeImageFileAsUrl = image => {
-    return new Promise(resolve=>{
-    const file = image;    
-
-    const reader = new FileReader();
-    //Like this
-    reader.onload = (e) => {resolve(e.target.result);} 
-    
-    const imageAsUrl = reader.readAsDataURL(file);
-    // console.log("1) imageAsUrl(readDataAsUrl) = " + imageAsUrl);
-
-    return imageAsUrl;
-  });
-} 
-
   handleChange =(e) => {
     this.setState({ [e.target.name]: e.target.value});
   }
 
+  fileChangedHandler = e => {
+    e.preventDefault();
+    const file = e.target.files[0];
+    this.setState({profilePicture: file});
+    console.log(this.state.profilePicture);
+  }
 
 
    Submit =(e)=> {
     e.preventDefault();
       if(this.state.name==="" || this.state.age==="" || this.state.course==="" || this.state.location==="" || this.state.intrests===""||this.state.year===""){
-      alert("Please enter details for every field");
-        return;
-      }
+    alert("ERRor");
+      return;
+   }
     
     var ref=fire.database().ref('User'); 
     var newRef=ref.push();
@@ -69,14 +47,9 @@ class UserSection extends Component {
       Course: this.state.course,
       Year: this.state.year,
       Location: this.state.location,
-      Interests: this.state.intrests,
-      ProfilePicture: this.state.profilePicture
+      Interests: this.state.intrests 
     
-    });
-
-    this.setState({
-      userHasSubmitted : !(this.state.userHasSubmitted)
-    });
+});
     
       ref.on("value",function(snapshot){
      
@@ -97,7 +70,7 @@ class UserSection extends Component {
       var year=t.Year;
       var intrests=t.Intrests;
       var r= document.getElementById("lol");
-      // console.log(name);
+      console.log(name);
      
       //var b=React.Children.toArray(name);    ///split into array
       var b2=React.Children.toArray(age);
@@ -203,8 +176,7 @@ class UserSection extends Component {
         course: "",
         year: "",
         location: "",
-        intrests: "",
-        userHasSubmitted: false
+        intrests: ""
       });
         document.getElementById("info").reset();//reset value after entered
         
@@ -227,8 +199,11 @@ var swap = window.setInterval(func, interval);
 }
 */
  
-clickPopUp=()=>{
-      
+clickPopUp=(e)=>{
+  e.preventDefault();
+  this.setState({
+    clickPopUp: !(this.state.clickPopUp)
+  });
  var close = document.getElementsByClassName("closebtn");//need to hide this pop up a the begining
 
   for (var i = 0; i < close.length; i++) {
@@ -250,107 +225,99 @@ clickPopUp=()=>{
  
   render() {
     
-    return (
-      
-    <div >
-
-    <Alert> 
-    <span className="closebtn" onClick={this.clickPopUp}>&times;</span>
+return (
   
-    <h4 className="alert-heading"><strong>Well Done!</strong></h4>
-    <hr className="w3-border-black"/>
-    <p>You successfully Created Your Profile.</p>
-    <h4/>
-        
-    </Alert>
-        
-        <div className="container2">
+ <div >
 
-        <link href='http://fonts.googleapis.com/css?family=Great+Vibes' rel='stylesheet' type='text/css'></link>
+  <Alert>
+    <span className="closebtn" onClick={this.clickPopUp}>&times;</span>
+   
+  <h4 className="alert-heading"><strong>Well Done!</strong></h4>
+   <hr className="w3-border-black"/>
+    <p>You successfully Created Your Profile.</p>
+  <h4/>
+      
+  </Alert>
+     
+    <div className="container2">
+
+    <link href='http://fonts.googleapis.com/css?family=Great+Vibes' rel='stylesheet' type='text/css'></link>
+    
+    <div className="form-group col-md-10">
+    <form onSubmit={this.Submit} id="info">
+    <h3 id="header">Profile Details</h3>
         
-        <div className="form-group col-md-10">
-        <form onSubmit={this.Submit} id="info">
-        <h3 id="header">Profile Details</h3>
-            
-      <div className="form-row">
-        <div className="form-group col-md-6" >
-          <label htmlfor="inputName" style={{color:"#d8d8d8"}}>Name</label>
+  <div className="form-row">
+    <div className="form-group col-md-6" >
+      <label htmlfor="inputName" style={{color:"#d8d8d8"}}>Name</label>
+      <input onChange={this.handleChange} className="form-control" type="text" name="name" placeholder="Enter Name"/>
+      {this.state.name ? <span style={{color: "#66cc00"}}>That's Good!</span> :<span style={{color: "#ff8080"}}>.</span>}
+    </div>
+    <div className="form-group col-md-6">
+      <label htmlfor="inputAge" style={{color:"#d8d8d8"}}>Age</label>
+      <input onChange={this.handleChange} className="form-control" type="text" id="age" name="age" placeholder="Enter Age"/>
+      {this.state.age ? <span style={{color: "#66cc00"}}>That's Good!</span> :<span style={{color: "#ff8080"}}>.</span>}
+
+    </div>
+  </div>
+  <div className="form-group">
+    <label htmlfor="inputLocation" style={{color:"#d8d8d8"}}>Location</label>
+    <input onChange={this.handleChange} className="form-control" type="text" id="location" name="location" placeholder="Enter Location"/>
+    {this.state.location ? <span style={{color: "#66cc00"}}>That's Good!</span> :<span style={{color: "ff8080"}}>.</span>}
+    </div>
+         {/* <div className="form-group col-md-6">
+          <label htmlFor="inputName" style={{color:"rgb(224, 224, 110)"}}>Name</label>
           <input onChange={this.handleChange} className="form-control" type="text" name="name" placeholder="Enter Name"/>
           {this.state.name ? <span style={{color: "#66cc00"}}>That's Good!</span> :<span style={{color: "#ff0000"}}></span>}
-        </div>
-        <div className="form-group col-md-6">
-          <label htmlfor="inputAge" style={{color:"#d8d8d8"}}>Age</label>
+        </div> */}
+
+        {/* <div className="form-group col-md-6">
+          <label htmlfor="inputAge" style={{color:"rgb(224, 224, 110)"}}>Age</label>
           <input onChange={this.handleChange} className="form-control" type="text" id="age" name="age" placeholder="Enter Age"/>
           {this.state.age ? <span style={{color: "#66cc00"}}>That's Good!</span> :<span style={{color: "#ff0000"}}></span>}
+        </div>  */}
 
+
+         {/* <div className="form-group">
+          <label htmlfor="inputLocation" style={{color:"rgb(224, 224, 110)"}}>Location</label>
+          <input onChange={this.handleChange} className="form-control" type="text" id="location" name="location" placeholder="Enter Location"/>
+          {this.state.location ? <span style={{color: "#66cc00"}}>That's Good!</span> :<span style={{color: "#ff0000"}}></span>}
+        </div>   */}
+    
+        <div className="form-group">
+          <label htmlfor="inputYear" style={{color:"#d8d8d8"}}>Year</label>
+          <input onChange={this.handleChange} className="form-control" type="text" id="year" name="year" placeholder="Enter College Year"/>
+          {this.state.year ? <span style={{color: "#66cc00"}}>That's Good!</span> :<span style={{color: "#ff8080"}}>.</span>}
         </div>
-      </div>
-      <div className="form-group">
-        <label htmlfor="inputLocation" style={{color:"#d8d8d8"}}>Location</label>
-        <input onChange={this.handleChange} className="form-control" type="text" id="location" name="location" placeholder="Enter Location"/>
-        {this.state.location ? <span style={{color: "#66cc00"}}>That's Good!</span> :<span style={{color: "#ff0000"}}></span>}
-      </div>
-            {/* <div className="form-group col-md-6">
-              <label htmlFor="inputName" style={{color:"rgb(224, 224, 110)"}}>Name</label>
-              <input onChange={this.handleChange} className="form-control" type="text" name="name" placeholder="Enter Name"/>
-              {this.state.name ? <span style={{color: "#66cc00"}}>That's Good!</span> :<span style={{color: "#ff0000"}}></span>}
-            </div> */}
 
-            {/* <div className="form-group col-md-6">
-              <label htmlfor="inputAge" style={{color:"rgb(224, 224, 110)"}}>Age</label>
-              <input onChange={this.handleChange} className="form-control" type="text" id="age" name="age" placeholder="Enter Age"/>
-              {this.state.age ? <span style={{color: "#66cc00"}}>That's Good!</span> :<span style={{color: "#ff0000"}}></span>}
-            </div>  */}
-
-
-            {/* <div className="form-group">
-              <label htmlfor="inputLocation" style={{color:"rgb(224, 224, 110)"}}>Location</label>
-              <input onChange={this.handleChange} className="form-control" type="text" id="location" name="location" placeholder="Enter Location"/>
-              {this.state.location ? <span style={{color: "#66cc00"}}>That's Good!</span> :<span style={{color: "#ff0000"}}></span>}
-            </div>   */}
-        
-            <div className="form-group">
-              <label htmlfor="inputYear" style={{color:"#d8d8d8"}}>Year</label>
-              <input onChange={this.handleChange} className="form-control" type="text" id="year" name="year" placeholder="Enter College Year"/>
-              {this.state.year ? <span style={{color: "#66cc00"}}>That's Good!</span> :<span style={{color: "#ff0000"}}></span>}
-            </div>
-
-      
-      <div className="form-row">
-      <div className="form-form-group col-md-6">
-        <label htmlFor="inputCourse" style={{color:"#d8d8d8"}}>Course</label>
-        <input onChange={this.handleChange} className="form-control" type="text" id="course" name="course" placeholder="Enter Course"/>
-        {this.state.course ? <span style={{color: "#66cc00"}}>That's Good!</span> :<span style={{color: "#ff0000"}}></span>}
-
-          </div>
-      
-      <div className="form-row">
-        <div className="form-group col-md-6">
-          <label htmlFor="inputIntrests" style={{color:"#d8d8d8"}}>Interests</label>
-          <input onChange={this.handleChange} className="form-control" type="text" name="intrests" id="intrests" placeholder="Enter Interests"/>
-          {this.state.intrests ? <span style={{color:"#66cc00"}}>That's Good!</span> :<span style={{color: "#ff0000"}}></span>}
-        </div>
-      </div>
-      <div className="form-row">
-      <div>
-          <label htmlFor="profilePicture" style={{color:"#d8d8d8"}}>Upload a Profile Picture</label>
-          <input onChange={this.handleImageUpload} className="form-control" type="file" name="profilePicture" id="profilePicture" placeholder="Upload a profle picture"/>
-          {this.state.intrests ? <span style={{color:"#66cc00"}}>That's Good!</span> :<span style={{color: "#ff0000"}}></span>}
-      </div>
+  
+  <div className="form-row">
+  <div className="form-form-group col-md-6">
+    <label htmlfor="inputCourse" style={{color:"#d8d8d8"}}>Course</label>
+    <input onChange={this.handleChange} className="form-control" type="text" id="course" name="course" placeholder="Enter Course"/>
+    {this.state.course ? <span style={{color: "#66cc00"}}>That's Good!</span> :<span style={{color: "#ff0000"}}>.</span>}
 
       </div>
-    </div>
-    <button className="btn btn-primary btn-success" type="submit"id="btn"><span class="glyphicon glyphicon-floppy-disk"></span> Save </button> 
-
-        </form>
-        
+  
+  <div className="form-row">
+    <div className="form-group col-md-6">
+      <label htmlfor="inputIntrests" style={{color:"#d8d8d8"}}>Interests</label>
+      <input onChange={this.handleChange} className="form-control" type="text" name="intrests" id="intrests" placeholder="Enter Interests"/>
+      {this.state.intrests ? <span style={{color:"#66cc00"}}>That's Good!</span> :<span style={{color: "#ff0000"}}>.</span>}
     </div>
     </div>
-    </div>  
+</div>
+ <button className="btn btn-primary btn-success" type="submit"id="btn"><span class="glyphicon glyphicon-floppy-disk"></span> Save </button> 
+
+		</form>
+    
+</div>
+</div>
+</div>  
 
 
-        );
-        
-    }
+    );
+    
   }
+}
 export default UserSection;
